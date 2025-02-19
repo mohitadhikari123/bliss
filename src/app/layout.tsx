2738bf0bf9ccfd43c05258e7a26a6f4200f1f2ee
mohitadhikari123 <111336786+mohitadhikari123@users.noreply.github.com>
2025-02-19 19:47:28 +0530

    update metadata in layout.tsx for improved SEO; replace meditationThumbnail image;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,9 @@ import { Metadata } from 'next';
 import { ReduxProvider } from './ReduxProvider';
 
 export const metadata: Metadata = {
-    title: "Bliss",
-    description: ""
+    title: "Bliss - A Virtual Meditation App",
+    description: "Bliss - A Virtual Meditation App is a serene digital space designed to help users achieve mindfulness through guided meditations, ambient soundscapes, and personalized relaxation techniques. Experience tranquility anytime, anywhere.",
+    keywords: "Bliss, Virtual Meditation App, Mindfulness, Guided Meditation, Relaxation, Stress Relief, Mental Wellness, Sleep Aid, Calmness, Breathing Exercises, Yoga, Meditation Music, Focus, Anxiety Relief, Self-care, Wellness App, Meditation for Beginners, Mindful Living, Daily Meditation, Peaceful Mind, Meditative Sounds, Tranquility, Zen, Meditation Timer, Relaxing Sounds, Inner Peace, Spiritual Growth, Meditation Coaching, Healing, Positive Energy, Well-being, Serenity, Meditation Sessions, Meditation Instructor, Online Meditation Instructor"
 };
 
 export default function RootLayout({
